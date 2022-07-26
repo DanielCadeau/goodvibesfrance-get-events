46c0b0363f07a83fdf8afc1ae38725c9c2ec8f9f
daniel cadeau <daniel1.cadeau@epitech.eu>
2022-07-26 15:55:59 +0200

    added more details on homepage

--- app/main.py
+++ app/main.py
@@ -5,7 +5,22 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Good Vibes France": "Hello World! 🤗 This API is used to feel good vibes in France. You can find documentation here: api.goodvibesfrance.live/redoc"}
+    return {
+        "Good Vibes France": "Hello World! 🤗 This API is used to feel good vibes in France.",
+        "Documentation": "You can find documentation here: http://api.goodvibesfrance.live/redoc",
+        "Details": "This is a school project developed by 3 students of Epitech, Paris. The goal of this project is to create a platform where people can feel good vibes in France. We don't use data for commercial purposes, but we use it to make the platform more interesting. We hope you enjoy it!",
+        "Contact": "Please reach out if you want to remove your data from the platform or if you have any questions.",
+        "Email": {
+            "Daniel Cadeau - developer of this api": "daniel1.cadeau@epitech.eu",
+        },
+        "Source": "http://api.goodvibesfrance.live/source",
+    }
+
+@app.get("/source")
+def read_source():
+    return {
+        "Github": "https://github.com/DanielCadeau/goodvibesfrance-get-events"
+    }
 
 @app.get("/cinemas/{city}")
 async def read_cinemas(city):
